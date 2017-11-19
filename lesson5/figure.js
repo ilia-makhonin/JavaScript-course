d92ea24139e909c9284882864d7ceba3e96500b0
DevelopUser <iljamahonin97@mail.ru>
2017-11-20 02:09:33 +0300

    Фигура

---
+++ lesson5/figure.js
@@ -0,0 +1,49 @@
+var white_figure = [
+    "figure/white/pawn.png", "figure/white/rook.png", "figure/white/bishop.png",
+    "figure/white/knight.png", "figure/white/queen.png", "figure/white/king.png"
+];
+
+var black_figure = [
+    "figure/black/pawn.png", "figure/black/rook.png", "figure/black/bishop.png",
+    "figure/black/knight.png", "figure/black/queen.png", "figure/black/king.png"
+];
+
+var letters = [ "A", "B", "C", "D", "E", "F", "J", "H"];
+
+
+Figure(black_figure);
+
+function Figure() {
+    /*for (var i = 0; i < 8; i++) {
+        var position = letters[i] + 7;
+
+        var white = document.getElementById(position);
+        var img = document.createElement("img");
+        img.setAttribute("src", white_figure[0]);
+        white.appendChild(img);
+    }*/
+
+    for (var j = 0; j < 8; j++) {
+        var num_position = j + 1;
+
+        for (var l = 0; l < 8; l++) {
+            var position = letters[l] + num_position;
+
+            if (num_position === 2) {
+                FigureColor(position, white_figure, 0);
+            } else if (num_position === 7) {
+                FigureColor(position, black_figure, 0);
+            }
+        }
+    }
+
+    var but = document.getElementById("gambit");
+    document.body.removeChild(but);
+}
+
+function FigureColor(position, color, num) {
+    var figure = document.getElementById(position);
+    var img = document.createElement("img");
+    img.setAttribute("src", color[num]);
+    figure.appendChild(img);
+}
