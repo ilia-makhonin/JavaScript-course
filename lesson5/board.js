3a5f3fa85c7a15b7c3c366fdd3f72b84f621e4ed
DevelopUser <iljamahonin97@mail.ru>
2017-11-20 02:13:07 +0300

    Генерация шахматной доски.

--- lesson5/board.js
+++ lesson5/board.js
@@ -1,64 +1,53 @@
-window.onload = function() {
+/*window.onload = function() {
 
     ChessBoard();
 
-    function ChessBoard() {
+};*/
+
+ChessBoard();
+
+function ChessBoard() {
                                             //Создаём тег TABLE
 
-        var table_board = document.createElement("table");
-        table_board.className = "chess-board";
-        document.body.appendChild(table_board);
+    var table_board = document.createElement("table");
+    table_board.className = "chess-board";
+    document.body.appendChild(table_board);
 
-        var letters = ["", "A", "B", "C", "D", "E", "F", "J", "H"];
+    var letters = ["", "A", "B", "C", "D", "E", "F", "J", "H"];
 
-                                        //Генерация самой шахматной доски
+                                        //Генерация шахматной доски
 
-        for (var i = 1; i <= 9; i++) {
+    for (var i = 0; i < 10; i++) {
+        var row = document.createElement("tr");
+        table_board.appendChild(row);
 
-                                        //Генерация строк и их нумерация
-
-            var row = document.createElement("tr");
-            table_board.appendChild(row);
-
-            for (var j = 0; j < 9; j++) {
-
-                            //Генерация отдельных клеток, определение их цвета и нумерация столбцов
-
-                if (i < 9) {
-                    if (j === 0) {
-                        var side_markup = document.createElement("td");
-                        side_markup.className = "side-markup";
-                        side_markup.innerText = 9 - i;
-                        row.appendChild(side_markup);
-                    } else {
-                        OneCell(i, j, row);
-                    }
-                } else {
-                    LastRow(j, letters, row);
-                }
+        for (var j = 0; j < 10; j++) {
+            if (i === 0 || i === 9) {
+                LettersRow(j, letters, row);
+            } else {
+                BoardCell(i, j, row, letters);
             }
         }
     }
-};
-
-                                    //Создаём одну клетку и выбираем ей цвет
-
-function OneCell(i, j, row) {
-    var cell = document.createElement("td");
-
-    if (i % 2 === 0 && j % 2 === 0 || i % 2 !== 0 && j % 2 !== 0) {
-        cell.className = "cell-white";
-    } else {
-        cell.className = "cell-black";
-    }
-
-    row.appendChild(cell);
 }
 
-                            //Создаём последнюю строку с буквенными обозначениями столбцов
+                                        //Создаём доску и номеруем строки
 
-function LastRow(j, letters, row) {
-    if (j === 0) {
+function BoardCell(i, j, row, letters) {
+    if (j === 0 || j === 9) {
+        var side_markup = document.createElement("td");
+        side_markup.className = "side-markup";
+        side_markup.innerText = 9 - i;
+        row.appendChild(side_markup);
+    } else {
+        OneCell(i, j, row, letters);
+    }
+}
+
+                            //Создаём первую и последнюю строки с буквенными обозначениями столбцов
+
+function LettersRow(j, letters, row) {
+    if (j === 0 || j === 9) {
         var angle = document.createElement("td");
         angle.className = "angle";
         row.appendChild(angle);
@@ -68,4 +57,21 @@ function LastRow(j, letters, row) {
         bottom_markup.innerText = letters[j];
         row.appendChild(bottom_markup);
     }
+}
+
+                                    //Создаём одну клетку, присваиваем id и выбираем ей цвет
+
+function OneCell(i, j, row, letters) {
+    var cell_id = letters[j] + (9 - i);
+
+    var cell = document.createElement("td");
+    cell.id = cell_id;
+
+    if (i % 2 === 0 && j % 2 === 0 || i % 2 !== 0 && j % 2 !== 0) {
+        cell.className = "cell-white";
+    } else {
+        cell.className = "cell-black";
+    }
+
+    row.appendChild(cell);
 }
